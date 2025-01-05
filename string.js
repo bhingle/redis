5c879e04c9fbcf6db3295ea18512471e8d386d3a
bhingle <abhishekbhingle@gmail.com>
2025-01-05 18:34:28 -0500

    redis implementation

---
+++ string.js
@@ -0,0 +1,8 @@
+const client = require("./client");
+async function init() {
+    await client.set("msg:6", "hey from nodejs");
+    const result = await client.get("msg:6");
+    console.log("result -> ",result);
+    
+}
+init();
